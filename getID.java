90f0597a534137bf261b1433494ebbfb2dedfe31
Clifnich <545903979@qq.com>
2017-02-02 17:54:52 -0500

    add getID.java

---
+++ getID.java
@@ -0,0 +1,17 @@
+public class getID {
+	public static void main(String[] args) {
+		Runnable r = () -> {
+			System.out.println("ID: " + getid());
+		};
+		Thread t1 = new Thread(r, "One");
+		Thread t2 = new Thread(r, "Two");
+		t1.start();
+		t2.start();
+	}
+
+	public static int getid() {
+		return counter++;
+	}
+
+	private static int counter = 1;
+}
